877728674d0de5891b1587d517ee9ef41704267e
Zhiyi Zhang <kzhue999@163.com>
2025-04-28 15:37:44 +0800

    本地服务发布成rpc服务

--- example/callee/userservice.cc
+++ example/callee/userservice.cc
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <string>
+#include "user.pb.h"
 
 /*
 UserService原来是一个本地服务，提供了两个进程内的本地方法，Login和GetFriendLists
 */
-class UserService {
+class UserService : public example::UserServiceRpc { // 使用在rpc服务发布端（rpc提供者）
 public:
     bool Login(std::string name, std::string pwd) {
         std::cout << "doing local service: Login" << std::endl;
         std::cout << "name: " << name << std::endl;
         std::cout << "pwd: " << pwd << std::endl;
+        return true;
+    }
+    /*
+    重写基类UserServiceRpc的虚函数 下面这些方法都是框架直接调用的
+    1. caller   ===>   Login(LoginRequest)   ===>   muduo   ===>   callee
+    2. callee   ===>   Login(LoginRequest)   ===>   交到下面重写的这个Login方法上了
+    */
+    void Login(::google::protobuf::RpcController* controller,
+               const example::LoginRequest* request,
+               example::LoginResponse* response,
+               ::google::protobuf::Closure* done) {
+        // 框架给业务上报了请求参数LoginRequest， 应用获取响应数据做本地业务
+        std::string name = request->name();
+        std::string pwd = request->pwd();
+
+        // 做本地业务
+        bool login_result = Login(name, pwd);
+
+        // 把响应写入 包括错误码、错误消息、返回值
+        example::ResultCode* code = response->mutable_result();
+        code->set_errcode(0);
+        code->set_errmsg("");
+        response->set_success(login_result);
+
+        // 执行回调操作   执行响应对象数据的序列号和网络发送（都是由框架来完成的）
+        done->Run();
     }
 };
+
+int main() {
+
+    return 0;
+}
